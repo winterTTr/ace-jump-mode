68a15edd4b3b9cfa61e3110bb6a06c6af0dea041
Derek Thurn <derek@thurn.ca>
2013-02-19 13:12:15 -0800

    Corrected copyright header
    The statement "This file is part of GNU Emacs" is intended for elisp that's distributed with emacs itself.

--- ace-jump-mode.el
+++ ace-jump-mode.el
@@ -7,7 +7,7 @@
 ;; Version  : 2.0.RC
 ;; Keywords : motion, location, cursor
 
-;; This file is part of GNU Emacs.
+;; This file is NOT part of GNU Emacs.
 
 ;; GNU Emacs is free software: you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
